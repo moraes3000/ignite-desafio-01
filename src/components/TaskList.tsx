ac8790000be6a9e46f865cf5b0464e67b0bb6523
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-10 20:15:13 -0300

    Challenge#01 -  ignite

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,19 +13,45 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [isComplete, setIsComplete] = useState(false);
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      alert('Please fill out a task')
+      return;
+    }
+
+    const addTask = {
+      id: Math.floor(Math.random() * 100),
+      title: newTaskTitle,
+      isComplete     
+    }
+
+    setTasks(oldState => [...oldState, addTask]);
+    setNewTaskTitle('');
+
   }
 
-  function handleToggleTaskCompletion(id: number) {
+  function handleToggleTaskCompletion(id: number ) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const endTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(endTasks) 
+    
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
-  }
+    // Remova uma task da listagem pelo ID   
+    const filterTask =tasks.filter(task =>task.id!=id)
 
+    setTasks(filterTask)
+
+  }
+  
   return (
     <section className="task-list container">
       <header>
